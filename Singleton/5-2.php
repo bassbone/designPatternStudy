677aaf4fb11bc28cb86e5c10e492f20c0ed7e683
J.I <bassbone0o_o0@yahoo.co.jp>
2015-02-09 23:38:36 +0900

    Singleton 5-2 2

--- Singleton/5-2.php
+++ Singleton/5-2.php
@@ -15,6 +15,9 @@ class Triple {
         $this->count = 0;
     }
     public static function getInstance($id) {
+        if ($id >= 3) {
+            throw new Exception("インスタンスは3個までです");
+        }
         if (!isset(self::$instance[$id])) {
             self::$instance[$id] = new Triple($id);
         }
@@ -31,22 +34,28 @@ class Main {
         
     }
     public static function main() {
-        $instance0 = Triple::getInstance(0);
-        $instance0->printValue();
-        $instance1 = Triple::getInstance(1);
-        $instance1->printValue();
-        $instance2 = Triple::getInstance(2);        
-        $instance2->printValue();
-        $instance3 = Triple::getInstance(0);
-        $instance3->printValue();
-        $instance4 = Triple::getInstance(1);
-        $instance4->printValue();
-        $instance5 = Triple::getInstance(2);        
-        $instance5->printValue();
-        $instance2->printValue();
-        $instance2->printValue();
-        $instance2->printValue();
-        $instance2->printValue();
+        try {
+            $instance0 = Triple::getInstance(0);
+            $instance0->printValue();
+            $instance1 = Triple::getInstance(1);
+            $instance1->printValue();
+            $instance6 = Triple::getInstance(3);
+            $instance6->printValue();
+            $instance2 = Triple::getInstance(2);        
+            $instance2->printValue();
+            $instance3 = Triple::getInstance(0);
+            $instance3->printValue();
+            $instance4 = Triple::getInstance(1);
+            $instance4->printValue();
+            $instance5 = Triple::getInstance(2);        
+            $instance5->printValue();
+            $instance2->printValue();
+            $instance2->printValue();
+            $instance2->printValue();
+            $instance2->printValue();
+        } catch (Exception $e) {
+            echo $e->getMessage()."<br>";
+        }
     }
 }
 
